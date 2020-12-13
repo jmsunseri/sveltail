56b2ea406ee8cbaa852374f99b97c54394f2f860
Justin Sunseri <jmsunseri@gmail.com>
2020-12-13 08:05:51 +0800

    add icon to button

--- src/Components/Button/Button.svelte
+++ src/Components/Button/Button.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
-  import type { FontWeight, FontSize } from '../../Font/font';
-  export let displayStyle: boolean;
-  export let text: string = '';
-  export let onClick: VoidFunction;
-  export let fontWeight: FontWeight;
-  export let fontSize: FontSize;
   export let styles: string[];
-
-  $: buttonFontWeight = fontWeight ? `font-${fontWeight}` : '';
-  $: buttonFontSize = fontSize ? `text-${fontSize}` : '';
-
-  $: buttonClass = `btn py-2 px-4 ${
-    !!styles?.length && styles.join(' ')
-  } ${buttonFontWeight} ${buttonFontSize}`;
+  export let iconStyles: string[];
 </script>
 
-<button class={buttonClass} on:click={onClick}>{text}</button>
-{#if displayStyle}
-  <pre>{buttonClass}</pre>
-{/if}
+<button class={`btn ${styles.join(' ')}`} on:click>
+  <div class="flex flex-row gap-1 items-center">
+    {#if $$slots.icon}
+      <div class={iconStyles.join(' ')}>
+        <slot name="icon" />
+      </div>
+    {/if}
+    <slot />
+    {#if $$slots.trailingIcon}
+      <div class={iconStyles.join(' ')}>
+        <slot name="trailingIcon" />
+      </div>
+    {/if}
+  </div>
+</button>
