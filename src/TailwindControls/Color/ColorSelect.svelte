f6715c20ba4d6f22da3a009f31d12d99f65038f3
Justin Sunseri <jmsunseri@gmail.com>
2020-12-23 12:15:01 +0800

    adding gradient functionality

--- src/TailwindControls/Color/ColorSelect.svelte
+++ src/TailwindControls/Color/ColorSelect.svelte
@@ -1,32 +1,62 @@
 <script lang="ts">
-  import { slide } from 'svelte/transition';
-  import FaPalette from 'svelte-icons/fa/FaPalette.svelte';
-  import IconButton from '../../Components/Button/IconButton.svelte';
+  import { onMount, tick } from 'svelte';
+  import { selectStyles } from '../../StyleDefinitions/SveltailStyles';
+  import Select from '../../Components/Select/Select.svelte';
+  import SelectOption from '../../Components/Select/SelectOption.svelte';
+  import Switch from '../../Components/Switch.svelte';
   import ColorMenu from './ColorMenu.svelte';
-  export let init: string;
   export let value: string;
   export let prefix: string;
 
-  let isShowPalette: boolean = false;
+  let from: string;
+  let to: string;
+  let isGradient: boolean = false;
+  let direction: string = 'b';
+  let select: Select;
 
-  const onShowPalletteClick = () => {
-    isShowPalette = !isShowPalette;
-  };
+  $: {
+    if (isGradient && from && to && direction) {
+      value = `${prefix}-gradient-to-${direction} ${from} ${to}`;
+    }
+  }
+
+  onMount(async () => {
+    await tick();
+
+    if (value.includes('gradient')) {
+      isGradient = true;
+      if (value.includes('gradient-to-b')) {
+        direction = 'b';
+      } else if (value.includes('gradient-to-t')) {
+        direction = 't';
+      } else if (value.includes('gradient-to-l')) {
+        direction = 'l';
+      } else if (value.includes('gradient-to-r')) {
+        direction = 'r';
+      }
+    }
+  });
 </script>
 
-<div class="flex flex-col">
-  <IconButton
-    on:click={onShowPalletteClick}
-    styles={['h-5', 'w-5', 'border-none', 'active:border-none', 'fill-current', 'text-blue-900']}>
-    <FaPalette />
-  </IconButton>
-  <div>
-    {#if isShowPalette}
-      <div
-        transition:slide
-        class="flex flex-col h-72 absolute p-4 z-10 bg-white border shadow-md rounded-lg gap-1">
-        <ColorMenu {prefix} bind:value {init} />
-      </div>
-    {/if}
+<div class="flex flex-col gap-2">
+  <div class="flex flex-row gap-2">
+    <Switch bind:checked={isGradient} />
+    <span>Gradient ?</span>
   </div>
+
+  {#if isGradient}
+    Direction:
+    <Select bind:this={select} styles={selectStyles} bind:value={direction}>
+      <SelectOption {select} value="b">From Top To Bottom</SelectOption>
+      <SelectOption {select} value="t">From Bottom To Top</SelectOption>
+      <SelectOption {select} value="r">From Left To Right</SelectOption>
+      <SelectOption {select} value="l">From Right To Left</SelectOption>
+    </Select>
+    From:
+    <ColorMenu prefix={isGradient ? `from` : prefix} bind:value={from} />
+    To:
+    <ColorMenu prefix={isGradient ? `to` : prefix} bind:value={to} />
+  {:else}
+    <ColorMenu {prefix} bind:value />
+  {/if}
 </div>
