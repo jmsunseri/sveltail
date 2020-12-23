9d842cf7d206d0998a8b63f5a62c0dcaa52c3d07
Justin Sunseri <jmsunseri@gmail.com>
2020-12-23 12:14:36 +0800

    adding switch

---
+++ src/Components/Switch.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let checked = false;
+</script>
+
+<div
+  on:click={() => (checked = !checked)}
+  class={`w-12 h-6 rounded-full border-dashed border-2 border-gray-300 cursor-pointer flex flex-row items-center ${checked ? 'bg-blue-300  justify-end' : 'bg-gray-200 justify-start'}`}>
+  <input
+    type="checkbox"
+    {checked}
+    class="appearance-none h-4 w-4 bg-white rounded-full mx-1" />
+</div>
