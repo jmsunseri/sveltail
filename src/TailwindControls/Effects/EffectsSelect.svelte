244ce6f34d069cc0b3399bc4ac519edc57ea6cdf
Justin Sunseri <jmsunseri@gmail.com>
2021-01-08 16:05:01 +0800

    allowing deletion of effect and transform styles

--- src/TailwindControls/Effects/EffectsSelect.svelte
+++ src/TailwindControls/Effects/EffectsSelect.svelte
@@ -1,18 +1,38 @@
 <script lang="ts">
+  import { Trash } from 'tabler-icons-svelte';
   import { EffectsStyle } from './EffectsStyle';
   import BoxShadowSelect from './BoxShadowSelect.svelte';
   import OpacitySelect from './OpacitySelect.svelte';
   import Button from '../../Components/Button/Button.svelte';
-  import { primaryButton } from '../../StyleDefinitions/SveltailStyles';
+  import {
+    primaryButton,
+    tooltip,
+  } from '../../StyleDefinitions/SveltailStyles';
   import Switch from '../../Components/Switch.svelte';
+  import Tooltip from '../../Components/Tooltip/Tooltip.svelte';
+  import IconButton from '../../Components/Button/IconButton.svelte';
   export let value: EffectsStyle[] = [new EffectsStyle()];
+
+  const onDelete = (index: number) => {
+    value = value.filter((_, i) => i !== index);
+  };
 </script>
 
 <div class="flex flex-col gap-1">
-  {#each value as effect}
-    <div class="flex flex-row gap-2">
-      <Switch bind:checked={effect.isHover} />
-      <span>Apply On Hover?</span>
+  {#each value as effect, index}
+    <div class="flex justify-between">
+      <div class="flex flex-row gap-2">
+        <Switch bind:checked={effect.isHover} />
+        <span>Apply On Hover?</span>
+      </div>
+      <Tooltip styles={tooltip}>
+        <div slot="tooltip">Delete Effects</div>
+        <IconButton
+          on:click={() => onDelete(index)}
+          styles={['border-none', 'fill-current', 'text-blue-300', 'focus:outline-none']}>
+          <Trash />
+        </IconButton>
+      </Tooltip>
     </div>
 
     <BoxShadowSelect bind:value={effect.boxShadow} />
--- src/TailwindControls/Transform/TransformSelect.svelte
+++ src/TailwindControls/Transform/TransformSelect.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { Trash } from 'tabler-icons-svelte';
   import { TransformStyles } from './TransformStyles';
   import TransformOriginSelect from './TransformOriginSelect.svelte';
   import TransformScaleSelect from './TransformScaleSelect.svelte';
@@ -7,16 +8,35 @@
   import TransformSkewSelect from './TransformSkewSelect.svelte';
   import Switch from '../../Components/Switch.svelte';
   import Button from '../../Components/Button/Button.svelte';
-  import { primaryButton } from '../../StyleDefinitions/SveltailStyles';
+  import {
+    primaryButton,
+    tooltip,
+  } from '../../StyleDefinitions/SveltailStyles';
+  import Tooltip from '../../Components/Tooltip/Tooltip.svelte';
+  import IconButton from '../../Components/Button/IconButton.svelte';
 
   export let value: TransformStyles[] = [new TransformStyles()];
+
+  const onDelete = (index: number) => {
+    value = value.filter((_, i) => i !== index);
+  };
 </script>
 
 <div class="flex flex-col gap-1">
-  {#each value as transform}
-    <div class="flex flex-row items-center">
-      <Switch bind:checked={transform.onHover} />
-      On Hover?
+  {#each value as transform, index}
+    <div class="flex justify-between">
+      <div class="flex flex-row gap-2">
+        <Switch bind:checked={transform.onHover} />
+        <span>Apply On Hover?</span>
+      </div>
+      <Tooltip styles={tooltip}>
+        <div slot="tooltip">Delete Transform</div>
+        <IconButton
+          on:click={() => onDelete(index)}
+          styles={['border-none', 'fill-current', 'text-blue-300', 'focus:outline-none']}>
+          <Trash />
+        </IconButton>
+      </Tooltip>
     </div>
     <TransformOriginSelect bind:value={transform.origin} />
     <TransformScaleSelect bind:value={transform.scale} />
