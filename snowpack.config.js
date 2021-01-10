e4d3e8e67e0df7d7f5fa2c11fef773e25c39e533
Justin Sunseri <jmsunseri@gmail.com>
2021-01-10 17:05:11 +0800

    using rollup for prod builds

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
+    snowpack: { url: '/', static: true },
     src: { url: '/dist' },
   },
   plugins: [
